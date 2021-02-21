e723e17f57e38cb7ec1f3e97105a6688fd77e158
Laura <laur507p@stud.kea.dk>
2021-02-21 21:20:56 +0100

    added 2021 to footer and the year will now update automatically

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 window.addEventListener("load", sidenVises);
 
+const yearDOM = document.querySelector(".currentyear");
+const date = new Date();
+const year = date.getFullYear();
+
 function sidenVises() {
-    console.log("sidenVises");
+  console.log("sidenVises");
 
-    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+  yearDOM.textContent = year;
+  document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
-
 
 function toggleMenu() {
-    console.log("toggleMenu");
+  console.log("toggleMenu");
 
-    document.querySelector("#menu").classList.toggle("hide");
+  document.querySelector("#menu").classList.toggle("hide");
 
-    let erSkjult = document.querySelector("#menu").classList.contains("hide");
+  let erSkjult = document.querySelector("#menu").classList.contains("hide");
 
-    if (erSkjult == true) {
-        //        document.querySelector("#menuknap").textContent = "☰";
-        document.querySelector(".menuknapicon").src = "billeder/burgermenuicon.svg"
-    } else {
-        //        document.querySelector("#menuknap").textContent = "X";
-        document.querySelector(".menuknapicon").src = "billeder/luk_icon.svg"
-    }
-
+  if (erSkjult == true) {
+    //        document.querySelector("#menuknap").textContent = "☰";
+    document.querySelector(".menuknapicon").src = "billeder/burgermenuicon.svg";
+  } else {
+    //        document.querySelector("#menuknap").textContent = "X";
+    document.querySelector(".menuknapicon").src = "billeder/luk_icon.svg";
+  }
 }
 
-$('.logo').hover(
-    function () {
-        $('.nameFull').removeClass('hiddenn');
-    },
-    function () {
-        $('.nameFull').addClass('hiddenn');
-    }
+$(".logo").hover(
+  function () {
+    $(".nameFull").removeClass("hiddenn");
+  },
+  function () {
+    $(".nameFull").addClass("hiddenn");
+  }
 );
